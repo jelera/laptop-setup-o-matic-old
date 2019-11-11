595bdf7051c4506efc55a81f66d7736507161aa0
Jose Elera <jelera@gmail.com>
2019-11-10 21:08:50 -0600

    Update initial_setup.rb

--- install_scripts/initial_setup.rb
+++ install_scripts/initial_setup.rb
@@ -3,15 +3,18 @@
 
 require_relative 'lib/helpers'
 
-def initial_setup
-  package = 'ruby'
+def initial_setup_ubuntu
+  package = 'snapd'
 
+  subtitle 'Enabling Multiverse Repositories ...'
   system('sudo add-apt-repository multiverse')
+
+  subtitle 'Enabling Snap Packages ...'
   system("sudo apt install -y #{package}")
 end
 
 def main
-  initial_setup
+  initial_setup_ubuntu
 end
 
 main
