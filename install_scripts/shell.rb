fe07d399aea3b1d417069f8ceffcebed93778d00
Jose Elera <jelera@gmail.com>
2019-11-10 21:10:31 -0600

    Update shell.rb

---
+++ install_scripts/shell.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/ruby
+# frozen_string_literal: true
+
+require_relative './lib/helpers'
+require 'pry'
+
+def is_zsh_in_shells_file?
+  shells_file = '/etc/shells'
+  shell_path = `which zsh`.chomp
+
+  File.readlines(shells_file).any? do |line|
+    line.include? shell_path
+  end
+end
+
+def update_zsh_as_default
+  shell_path = `which zsh`.chomp
+  current_user = ENV['USER']
+
+  subtitle 'Changing the default shell to ZSH ...'
+
+  if is_zsh_in_shells_file?
+    subtitle "Adding #{shell_path} to /etc/shells"
+    system("sudo sh -c echo #{shell_path} >> /etc/shells")
+  end
+
+  system("sudo chsh -s #{shell_path} #{current_user}")
+end
+
+def set_zsh_as_default_shell
+  current_shell = ENV['SHELL']
+
+  case current_shell
+  when /zsh/
+    update_zsh_as_default if `which zsh`.chomp != '/usr/bin/zsh'
+  else
+    update_zsh_as_default
+  end
+end
+
+def install_zsh
+  install_packages(
+    ubuntu: %w[
+      zsh
+    ],
+    mac: %w[
+      zsh
+    ]
+  )
+end
+
+def main
+  subtitle 'Installing ZSH ...'
+  install_zsh
+
+  subtitle "Updating ZSH as default shell (if it isn't already)..."
+  set_zsh_as_default_shell
+end
+
+main
