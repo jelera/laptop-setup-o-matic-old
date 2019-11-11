4aaadbe6bf3d19516cb84cf70f159f31a8c5a2f1
Jose Elera <jelera@gmail.com>
2019-11-10 21:09:12 -0600

    Update helpers.rb

--- install_scripts/lib/helpers.rb
+++ install_scripts/lib/helpers.rb
@@ -5,10 +5,21 @@ require_relative './linux_helpers'
 
 require 'etc'
 require 'yaml'
+require 'colorize'
+require 'fileutils'
 require 'pry'
 
 def settings
   YAML.safe_load(File.read('../config.yml'))
+end
+
+def subtitle(text)
+  console_width = ENV['COLUMNS'].to_i
+  puts ""
+  puts ""
+  puts text.colorize(:light_cyan)
+  console_width.times { print '-'.colorize(:light_cyan) }
+  puts ""
 end
 
 def this_system
@@ -39,6 +50,7 @@ def install_packages(ubuntu: [], arch: [], mac: [])
   #   ""
   end
 end
+
 
 
 binding.pry
