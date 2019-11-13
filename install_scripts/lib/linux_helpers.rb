73add202a071af4a956868b338832743ac7df69d
Jose Elera <jelera@gmail.com>
2019-11-13 16:12:59 -0600

    Implement a proper install_packages

--- install_scripts/lib/linux_helpers.rb
+++ install_scripts/lib/linux_helpers.rb
@@ -20,14 +20,32 @@ module LinuxOS
     parse_release_file[:VERSION_ID]
   end
 
-  def self.install_packages(package_list)
+  def self.install_packages(ubuntu, snap, flatpak, arch, aur)
     install_command = ''
 
-    case distro
+    ubuntu_packages = ubuntu
+    snap_packages = snap
+    flat_packages = flatpak
+    pacman_packages = arch
+    aur_packages = aur
+
+    case LinuxOS.distro
     when 'ubuntu'
-      install_command = 'sudo apt install -y'
+      if !ubuntu_packages.empty?
+        install_command = "sudo apt install -y #{ubuntu_packages.join(' ')}"
+      elsif !snap_packages.empty?
+        install_command = "sudo snap install #{snap_packages.join(' ')}"
+      elsif !flat_packages.empty?
+        install_command = "flatpak install -y flathub #{flat_packages.join(' ')}"
+      end
+    when 'arch'
+      if !pacman_packages.empty?
+        install_command = "sudo pacman -S #{pacman_packages.join(' ')}"
+      else !aur_packages.empty?
+        install_command = 'echo building functionality'
+      end
     end
 
-    system("#{install_command} #{package_list.join(' ')} ")
+    system(install_command)
   end
 end
