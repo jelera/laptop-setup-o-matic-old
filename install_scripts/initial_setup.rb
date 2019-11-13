927817caa009dcd6d9a07569fa824789ae406f9b
Jose Elera <jelera@gmail.com>
2019-11-13 16:11:26 -0600

    Change initial_setup.rb

--- install_scripts/initial_setup.rb
+++ install_scripts/initial_setup.rb
@@ -3,18 +3,42 @@
 
 require_relative 'lib/helpers'
 
-def initial_setup_ubuntu
-  package = 'snapd'
+def install_flatpak
+  subtitle 'Installing Flatpak support ...'
 
+  flatpak_install_command = ''
+
+  if LinuxOS.distro == 'ubuntu'
+    if LinuxOS.version == '18.04'
+      system('sudo add-apt-repository -y ppa:alexlarsson/flatpak')
+      system('sudo apt update -y')
+    end
+
+    system('sudo apt install -y flatpak')
+    system('flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo')
+  end
+end
+
+def install_snappy
+  if LinuxOS.distro == "ubuntu"
+    subtitle 'Enabling Snap Packages ...'
+    system('sudo apt install -y snapd')
+  end
+end
+
+def initial_setup_ubuntu
   subtitle 'Enabling Multiverse Repositories ...'
   system('sudo add-apt-repository multiverse')
-
-  subtitle 'Enabling Snap Packages ...'
-  system("sudo apt install -y #{package}")
 end
 
 def main
-  initial_setup_ubuntu
+  if os_is_linux?
+    if LinuxOS.distro == "ubuntu"
+      initial_setup_ubuntu
+    end
+    install_snappy
+    install_flatpak
+  end
 end
 
 main
